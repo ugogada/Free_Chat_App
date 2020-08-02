10d904fd6b3d8dbbb22d560de3b9e1411579b0ed
Pranav <pkakadiya274@rku.ac.in>
2020-08-02 17:52:18 +0530

    splash screen added

---
+++ app/src/main/java/com/example/freechatapp/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.freechatapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread splash=new Thread()
+        {
+            public void run()
+            {
+                try {
+                    sleep(10*1000);
+                    Intent i=new Intent(getBaseContext(),MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        };
+        splash.start();
+    }
+}

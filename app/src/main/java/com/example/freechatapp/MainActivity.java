dbed880f588c068a5eb5839985885c6ff1231627
Pranav <pkakadiya274@rku.ac.in>
2020-08-02 19:33:32 +0530

    click action add to aboutus and contact button

--- app/src/main/java/com/example/freechatapp/MainActivity.java
+++ app/src/main/java/com/example/freechatapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.freechatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void contactus(View view) {
+        Intent i=new Intent(getBaseContext(),activity_contactus.class);
+        startActivity(i);
+    }
+
+    public void aboutus(View view) {
+        Intent i=new Intent(getBaseContext(),activity_aboutus.class);
+        startActivity(i);
     }
 }
